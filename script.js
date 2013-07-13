dd7d0a0080ccc398d57de99f11bc670561c0ab66
Samata <yayoi_samata@Yayoi-no-MacBook-Air.local>
2013-07-13 16:20:36 +0900

    #4

--- script.js
+++ script.js
@@ -1,11 +1,82 @@
 enchant();
 
-var IMG = ['img/bg.png','img/title.png','img/operation.png','img/stage01.png','img/stage02.png','img/stage03.png'];
+var IMG = ['img/bg.png','img/title.png','img/operation.png','img/stage01.png','img/stage02.png','img/stage03.png','images/icon0.png'];
 
 window.onload = function() {
     var game = new Game(320, 320);
     game.fps = 16;
+    game.tick = 0;
     game.preload(IMG);
+    
+    
+    var mainAPPLE = enchant.Class.create(enchant.Sprite,{
+        initialize: function(speed){
+            enchant.Sprite.call(this, 16, 16);
+            this.speed = speed;
+            this.x = Math.random() * 300;
+            this.y = -16;
+            this.addEventListener(Event.ENTER_FRAME, function() {
+                this.y += this.speed;
+        
+                /*//クマと衝突
+                if (bear.within(apple, 16)) {
+                    game.score+=30;
+                    scene.removeChild(apple); 
+                } 
+                //地面と衝突
+                else if (apple.y > 320 - 16) {
+                    scene.removeChild(apple); 
+                }*/
+            });
+        
+        }
+    });
+    
+    var Apple = enchant.Class.create(mainAPPLE,{
+        initialize: function(scene, speed){
+            mainAPPLE.call(this, speed);
+            this.image = game.assets[ IMG[6] ];
+            this.frame = 15;
+            scene.addChild(this);
+        }
+    });
+    
+    var NibbleApple = enchant.Class.create(mainAPPLE,{
+        initialize: function(scene, speed){
+            mainAPPLE.call(this, speed);
+            this.image = game.assets[ IMG[6] ];
+            this.frame = 12;
+            scene.addChild(this);
+        }
+    });
+    
+    /*//リンゴの追加
+    game.addApple = function(x, speed, scene) {
+        //リンゴの生成
+            var apple = new Sprite(16, 16);
+            apple.image = game.assets[ IMG[6] ];
+            apple.x = x;
+            apple.y = -16;
+            apple.frame = 15;
+            apple.speed = speed;
+        scene.addChild(apple);
+        
+        //スプライトの定期処理
+        apple.addEventListener(Event.ENTER_FRAME, function() {
+            apple.y += apple.speed;*/
+    
+            /*//クマと衝突
+            if (bear.within(apple, 16)) {
+                game.score+=30;
+                scene.removeChild(apple); 
+            } 
+            //地面と衝突
+            else if (apple.y > 320 - 16) {
+                scene.removeChild(apple); 
+            }
+        });*/
+    //};
+    
     
     game.onload = function () {
         var bg = new Sprite(320, 320);
@@ -45,13 +116,23 @@ window.onload = function() {
         var scene = new Scene ();
         var bg = new Sprite(320, 320);
         bg.image = game.assets[ IMG[3] ];
+        
         scene.addChild(bg);
-        bg.addEventListener(Event.TOUCH_START, function(e) {
-            game.pushScene(game.stage02());
+        // apple
+        bg.addEventListener(Event.ENTER_FRAME, function( ) {
+            if(game.tick % 12 == 0 ) {
+                var speed = 3 + Math.floor(Math.random() * 6 );
+                var speed02 = 3 + Math.floor(Math.random() * 4 );
+                new Apple(scene, speed);
+                new NibbleApple(scene, speed02);
+            }
+            game.tick++;
         });
+        
         return scene;
     };
     
+    /*
     //STAGE 2
     game.stage02 = function( ) {
         var scene = new Scene ();
@@ -71,7 +152,7 @@ window.onload = function() {
         bg.image = game.assets[ IMG[5] ];
         scene.addChild(bg);
         return scene;
-    };
-        
+    };*/
+
     game.start();
 };
