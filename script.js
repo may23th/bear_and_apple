9ebd706cc60aab0fafb9aa3db46e3147c8681727
Samata <yayoi_samata@Yayoi-no-MacBook-Air.local>
2013-07-13 11:28:16 +0900

    #2,#3

--- script.js
+++ script.js
@@ -1,42 +1,77 @@
 enchant();
+
+var IMG = ['img/bg.png','img/title.png','img/operation.png','img/stage01.png','img/stage02.png','img/stage03.png'];
+
 window.onload = function() {
-    //ゲームオブジェクトの生成
-    var game = new Game(320, 320);　// ゲーム画面サイズを指定してるよ。9leapに投稿するときの制約のサイズです
-    game.fps = 16; // 1秒間に実行される回数　fps = fream/second
-
-    //画像の読み込み
-    game.preload('http://enchantjs.com/assets/images/chara1.gif');
-
-    //ロード完了時に呼ばれる
-    game.onload = function() {
-        //スプライトの生成
-        var bear = new Sprite(32, 32); // 新しいスプライトの生成（サイズ）
-        bear.image = game.assets['http://enchantjs.com/assets/images/chara1.gif'];　// 画像の設定
-        bear.tick = 0;
-        bear.anim = [10, 11, 10, 12];
-        game.rootScene.addChild(bear);　// これをしないとくまは表示されないよ(╹◡╹)
-        
-        //スプライトの定期処理
-        bear.addEventListener(Event.ENTER_FRAME, function() {
-            //スプライトのフレームの指定
-            bear.tick++;
-            bear.frame = bear.anim[bear.tick % 4];
-            
-            //右向き
-            if (bear.scaleX == 1) {
-                bear.x += 5;
-                //向き変更
-                if (bear.x > 320 - 32) bear.scaleX = -1;
-            } 
-            //左向き
-            else {
-                bear.x -= 5;
-                //向き変更
-                if (bear.x < 0) bear.scaleX = 1;
-            }
+    var game = new Game(320, 320);
+    game.fps = 16;
+    game.preload(IMG);
+    
+    game.onload = function () {
+        var bg = new Sprite(320, 320);
+        bg.image = game.assets[ IMG[0] ];
+        bg.addEventListener(Event.TOUCH_START, function(e) {
+            game.pushScene(game.TitleScene());
         });
+        game.rootScene.addChild(bg);
     };
-
-    //ゲームの開始
+    
+    game.TitleScene = function( ) {
+        var scene = new Scene ();
+        var bg = new Sprite(320, 320);
+        bg.image = game.assets[ IMG[1] ];
+        scene.addChild(bg);
+        bg.addEventListener(Event.TOUCH_START, function(e) {
+            game.pushScene(game.OperationDescription());
+        });
+        game.rootScene.addChild(bg);
+    };
+    
+    game.OperationDescription = function( ) {
+        var scene = new Scene ();
+        var bg = new Sprite(320, 320);
+        bg.image = game.assets[ IMG[2] ];
+        scene.addChild(bg);
+        bg.addEventListener(Event.TOUCH_START, function(e) {
+            game.pushScene(game.stage01());
+        });
+        return scene;
+        
+    };
+    
+    
+    // STAGE 1
+    game.stage01 = function( ) {
+        var scene = new Scene ();
+        var bg = new Sprite(320, 320);
+        bg.image = game.assets[ IMG[3] ];
+        scene.addChild(bg);
+        bg.addEventListener(Event.TOUCH_START, function(e) {
+            game.pushScene(game.stage02());
+        });
+        return scene;
+    };
+    
+    //STAGE 2
+    game.stage02 = function( ) {
+        var scene = new Scene ();
+        var bg = new Sprite(320, 320);
+        bg.image = game.assets[ IMG[4] ];
+        scene.addChild(bg);
+        bg.addEventListener(Event.TOUCH_START, function(e) {
+            game.pushScene(game.stage03());
+        });
+        return scene;
+    };
+    
+    //STAGE 3
+    game.stage03 = function( ) {
+        var scene = new Scene ();
+        var bg = new Sprite(320, 320);
+        bg.image = game.assets[ IMG[5] ];
+        scene.addChild(bg);
+        return scene;
+    };
+        
     game.start();
 };
